208c85ba91104b57edf47870efc212fa4fca677b
Lamoara <jorginos40@gmail.com>
2025-01-19 17:12:26 +0100

    shaders updated

--- fragment_shader.glsl
+++ fragment_shader.glsl
@@ -1,12 +1,23 @@
 #version 330 core
 
 in vec3 FragPos;
+in vec3 Normal;
+in vec2 TexCoords;
 
-uniform vec3 camPos;
+uniform sampler2D texture1;
+uniform vec3 cameraPos;
+uniform vec4 fogColor;
+uniform float minDistance;
+uniform float maxDistance;
 
 out vec4 FragColor;
 
 void main() {
-    float distance = length(FragPos - camPos);
-    FragColor = vec4(1.0, 1.0, 1.0, 1.0) - vec4(1.0, 1.0, 1.0, 0.0) * distance * distance * 0.1;
+    float distance = length(FragPos - cameraPos);
+
+    float fogFactor = clamp((distance - minDistance) / (maxDistance - minDistance), 0.0, 1.0);
+
+    vec4 objectColor = texture(texture1, TexCoords);
+
+    FragColor = mix(objectColor, fogColor, fogFactor);
 }
